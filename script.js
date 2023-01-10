e69bdbb508ed5dc7c2b96caabafdf23a87c193f7
Maria Eduarda Massa <djsheepwolfbr@gmail.com>
2023-01-10 19:25:52 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var map = L.map('map').setView([51.505, -0.09], 13);
+var map = L.map('mapid').setView([51.505, -0.09], 13);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
