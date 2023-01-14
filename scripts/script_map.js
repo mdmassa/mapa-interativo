13ea6a8de9740f067aa619bdf4c446a9a23ef992
mdmassa <djsheepwolfbr@gmail.com>
2023-01-14 13:13:24 -0300

    Added information div on faerun_map

--- scripts/script_map.js
+++ scripts/script_map.js
@@ -36,39 +36,50 @@ var map = L.map('map', {
   // [Nome da Localização]
   var nomeMarker = L.marker([coordenada]).addTo(map);
   nomeMarker.bindPopup(`<h3>nome</h3>
-  <p>tipo</p>`)on('click', () => {
+  <p class="locDesc">tipo</p>`)on('click', () => {
     map.flyTo([coordenada], 6);
 }).addTo(map);
 
   */
+  var info = '/run/media/massa/FILES/projetos-dev/mapa-interativo/web/all_maps/dnd/forgotten_realms/continents/faerun/locations/inverno_remoto.txt';
 
   // Inverno Remoto -- Neverwinter
   var neverwinterMarker = L.marker([83.03155, -158.31299]).addTo(map);
   neverwinterMarker.bindPopup(`<h3>Inverno Remoto</h3> 
-  <p>Cidade</p>
+  <p class="locDesc">Cidade</p>
   `).on('click', () => {
       map.flyTo([83.03155, -158.31299], 6);
+      // add information from .txt
+      fetch(info)
+        .then(response => response.text())
+        .then(data => {
+          document.getElementById("mapInfo").innerHTML = data;
+      })
+
+  .catch(error => console.error(error));
+  
   }).addTo(map);
   
   // Phandalin
   var phandalinMarker = L.marker([82.66787727945918, -153.585859355341]).addTo(map);
   phandalinMarker.bindPopup(`<h3>Phandalin</h3> 
-  <p>Vila</p>
+  <p class="locDesc">Vila</p>
   `).on('click', () => {
       map.flyTo([82.66787727945918, -153.585859355341], 6);
+  
   }).addTo(map);
 
   // Porto Llast
   var portoLLastMarker = L.marker([83.21049936471866, -159.62942352832476]).addTo(map);
   portoLLastMarker.bindPopup(`<h3>Porto Llast</h3>
-  <p>Pequena Cidade</p>`).on('click', () => {
+  <p class="locDesc">Pequena Cidade</p>`).on('click', () => {
     map.flyTo([83.21049936471866, -159.62942352832476], 6);
 }).addTo(map);
 
   // Luskan
   var luskanMarker = L.marker([83.7107320137689, -161.9377135462538]).addTo(map);
   luskanMarker.bindPopup(`<h3>Luskan</h3>
-  <p>Cidade</p>`).on('click', () => {
+  <p class="locDesc">Cidade</p>`).on('click', () => {
     map.flyTo([83.7107320137689, -161.9377135462538], 6);
 }).addTo(map);
 
@@ -77,10 +88,11 @@ var map = L.map('map', {
 
   //#endregion
 
+
   // Mostrar coordenadas no console
   map.on('click', function(e){
     var coord = e.latlng;
     var lat = coord.lat;
     var lng = coord.lng;
-    console.log("coordenadas do clique: " + lat + ", " + lng);
+    console.log("coordenadas do clique: " + lat + ", " + lng + "\nLocal ID: " + currentLocation);
     });
