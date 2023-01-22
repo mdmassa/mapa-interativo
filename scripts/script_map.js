231db7249bcba8834c782b841c67df201efa14d7
mdmassa <djsheepwolfbr@gmail.com>
2023-01-22 20:50:57 -0300

    Added more markers

--- scripts/script_map.js
+++ scripts/script_map.js
@@ -43,7 +43,7 @@ var map = L.map('map', {
   //#region Inverno remoto
   var neverwinterMarker = L.marker([83.03155, -158.31299]).addTo(map);
   neverwinterMarker.bindPopup(`<h3>Inverno Remoto</h3> 
-  <p class="locDesc">Cidade</p>
+  <p class="locDesc">Cosmópole</p>
   `).on('click', () => {
       map.flyTo([83.03155, -158.31299], 6);
 
@@ -80,7 +80,7 @@ var map = L.map('map', {
   //#region Porto LLast
   var portoLLastMarker = L.marker([83.21049936471866, -159.62942352832476]).addTo(map);
   portoLLastMarker.bindPopup(`<h3>Porto Llast</h3>
-  <p class="locDesc">Pequena Cidade</p>`).on('click', () => {
+  <p class="locDesc">Cidade Pequena</p>`).on('click', () => {
     map.flyTo([83.21049936471866, -159.62942352832476], 6);
 
     // add information from .txt
@@ -97,7 +97,7 @@ var map = L.map('map', {
   //#region Luskan
   var luskanMarker = L.marker([83.7107320137689, -161.9377135462538]).addTo(map);
   luskanMarker.bindPopup(`<h3>Luskan</h3>
-  <p class="locDesc">Cidade</p>`).on('click', () => {
+  <p class="locDesc">Cidade Pequena</p>`).on('click', () => {
     map.flyTo([83.7107320137689, -161.9377135462538], 6);
 
     // add information from .txt
@@ -115,7 +115,7 @@ var map = L.map('map', {
 
   var mirabarMarker = L.marker([84.3995116971245, -152.3528140432342]).addTo(map);
   mirabarMarker.bindPopup(`<h3>Mirabar</h3>
-  <p class="locDesc">Cidade</p>`).on('click', () => {
+  <p class="locDesc">Cidade Pequena</p>`).on('click', () => {
     map.flyTo([84.3995116971245, -152.3528140432342], 6);
 
     // add information from .txt
@@ -133,7 +133,7 @@ var map = L.map('map', {
   //#region Sela Longa
   var longsaddleMarker = L.marker([83.55478564554016, -150.8579214601944]).addTo(map);
   longsaddleMarker.bindPopup(`<h3>Sela Longa</h3>
-  <p class="locDesc">Cidade</p>`).on('click', () => {
+  <p class="locDesc">Aldeia</p>`).on('click', () => {
     map.flyTo([83.55478564554016, -150.8579214601944], 6);
 
     // add information from .txt
@@ -147,6 +147,93 @@ var map = L.map('map', {
   }).addTo(map);
   //#endregion
 
+  //#region Triboar
+   var triboarMarker = L.marker([82.7681391822544, -147.12069000259504]).addTo(map);
+   triboarMarker.bindPopup(`<h3>Triboar</h3>
+   <p class="locDesc">Cidade Grande</p>`).on('click', () => {
+    map.flyTo([82.7681391822544, -147.12069000259504], 6);
+  
+    // add information from .txt
+        fetch("http://localhost:5500/web/all_maps/dnd/forgotten_realms/continents/faerun/locations/triboar/triboar.html")
+         .then(response => response.text())
+           .then(data => {
+            document.getElementById("mapInfo").innerHTML = data;
+            document.querySelector('#mapInfo').scrollTop = 0;
+        })
+  
+    }).addTo(map);
+    //#endregion
+
+  //#region Lariço Vermelho
+  var redlarchMarker = L.marker([82.40823147725087, -146.94482028694327]).addTo(map);
+  redlarchMarker.bindPopup(`<h3>Lariço Vermelho</h3>
+  <p class="locDesc">Vila</p>`).on('click', () => {
+    map.flyTo([82.40823147725087, -146.94482028694327], 6);
+  
+    // add information from .txt
+        fetch("http://localhost:5500/web/all_maps/dnd/forgotten_realms/continents/faerun/locations/larico_vermelho/larico_vermelho.html")
+        .then(response => response.text())
+          .then(data => {
+            document.getElementById("mapInfo").innerHTML = data;
+            document.querySelector('#mapInfo').scrollTop = 0;
+        })
+  
+    }).addTo(map);
+  //#endregion
+
+  //#region Águas Profundas
+  var waterdeepMarker = L.marker([81.65330835588037, -149.472947449437]).addTo(map);
+  waterdeepMarker.bindPopup(`<h3>Águas Profundas</h3>
+  <p class="locDesc">Cosmópole</p>`).on('click', () => {
+    map.flyTo([81.65330835588037, -149.472947449437], 6);
+  
+    // add information from .txt
+        fetch("http://localhost:5500/web/all_maps/dnd/forgotten_realms/continents/faerun/locations/aguas_profundas/aguas_profundas.html")
+        .then(response => response.text())
+          .then(data => {
+            document.getElementById("mapInfo").innerHTML = data;
+            document.querySelector('#mapInfo').scrollTop = 0;
+        })
+  
+    }).addTo(map);
+  //#endregion
+
+  //#region Pedra Noturna
+  var nightstoneMarker = L.marker([81.39093994309583, -145.88960199303287]).addTo(map);
+  nightstoneMarker.bindPopup(`<h3>Pedra Noturna</h3>
+  <p class="locDesc">Cosmópole</p>`).on('click', () => {
+    map.flyTo([81.39093994309583, -145.88960199303287], 6);
+  
+    // add information from .txt
+        fetch("http://localhost:5500/web/all_maps/dnd/forgotten_realms/continents/faerun/locations/aguas_profundas/aguas_profundas.html")
+        .then(response => response.text())
+          .then(data => {
+            document.getElementById("mapInfo").innerHTML = data;
+            document.querySelector('#mapInfo').scrollTop = 0;
+        })
+  
+    }).addTo(map);
+  //#endregion
+
+  //#region Yartar
+  var yartarMarker = L.marker([83.06605550417774, -144.40201887902353]).addTo(map);
+  yartarMarker.bindPopup(`<h3>Yartar</h3>
+  <p class="locDesc">Cidade Pequena</p>`).on('click', () => {
+    map.flyTo([83.06605550417774, -144.40201887902353], 6);
+  
+    // add information from .txt
+        fetch("http://localhost:5500/web/all_maps/dnd/forgotten_realms/continents/faerun/locations/aguas_profundas/aguas_profundas.html")
+        .then(response => response.text())
+          .then(data => {
+            document.getElementById("mapInfo").innerHTML = data;
+            document.querySelector('#mapInfo').scrollTop = 0;
+        })
+  
+    }).addTo(map);
+  //#endregion
+
+
+
   // markers pop-up
   neverwinterMarker.openPopup();
 
@@ -158,5 +245,5 @@ var map = L.map('map', {
     var coord = e.latlng;
     var lat = coord.lat;
     var lng = coord.lng;
-    console.log("coordenadas do clique: " + lat + ", " + lng + "\nLocal ID: " + currentLocation);
+    console.log("coordenadas do clique: " + lat + ", " + lng);
     });
